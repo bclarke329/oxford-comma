4edea18e769a8c2fbbf071c13cd7d94f40b134a5
bclarke329 <bmendoza329@hotmail.com>
2020-10-02 11:05:07 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")    
+    else 
+        array_1 = array[0..-2]
+        array_2 = array[-1]
+        array_1.join(', ') + ", and " + array_2
+    end 
 end
